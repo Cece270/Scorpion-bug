a558ec1e3cb7bc75ade4b57eaa5b51ac87198afd
Cece270 <enochoduviewanegere@gmail.com>
2024-07-22 14:33:13 +0100

    Update XeonBug6.js

--- XeonBug6.js
+++ XeonBug6.js
@@ -64,8 +64,8 @@ const ApkXeon = JSON.parse(fs.readFileSync('./database/autoreply/apk.json'))
 
 //time
 const xtime = moment.tz('Asia/Kolkata').format('HH:mm:ss')
-        const xdate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
-        const time2 = moment().tz('Asia/Kolkata').format('HH:mm:ss')  
+        const xdate = moment.tz('Africa/Nigeria').format('DD/MM/YYYY')
+        const time2 = moment().tz('Africa/Nigeria').format('HH:mm:ss')  
          if(time2 < "23:59:00"){
 var xeonytimewisher = `Good Night broski 🌌`
  }
